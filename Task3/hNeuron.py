610dfcb2e24c80dc37794cf46507994e09ebb872
Pavel Georgiev <pavel.r.georgiev@hotmail.com>
2017-04-02 15:05:51 +0100

    Task 3.3 DONE

--- Task3/hNeuron.py
+++ Task3/hNeuron.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 def hNeuron(W, X):
+    # Task 3.1
+    #  X: data matrix of N-by-D
+    #  W: weight vector of (D+1)-by-1
+    #  Y: output vector of N-by-1
     a = W.T.dot(X)
-    result_vector = (a > 0).astype(int)
-    return result_vector
+    Y = (a > 0).astype(int)
+    return Y
--- Task3/sNeuron.py
+++ Task3/sNeuron.py
@@ -1,5 +1,10 @@
 import numpy as np
 
 def sNeuron(W,X):
+    # Task 3.2
+    #  X: data matrix of N-by-D
+    #  W: weight vector of (D+1)-by-1
+    #  Y: output vector of N-by-1
     a = W.T.dot(X)
-    result_vector = 1.0 / (1 + np.exp(-1.0*a))
+    Y = 1.0 / (1 + np.exp(-1.0*a))
+    return Y
