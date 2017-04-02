0b2bf3a74718b63ffa88feb5b08a431de74ed005
Pavel Georgiev <pavel.r.georgiev@hotmail.com>
2017-04-03 00:42:56 +0100

    I have a plot now and its pretty

--- Task3/plot_regions_hNN_A.py
+++ Task3/plot_regions_hNN_A.py
@@ -1,19 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
+import matplotlib.patches as mpatches
 from hNN_A import hNN_A
 
+# Generate points between 0 and 10 to classify
 xplot = np.linspace(0,10, 2000)
 yplot = np.linspace(0,10,2000)
-print xplot.shape
 xx, yy = np.meshgrid(xplot, yplot)
-
 gridX = np.vstack((xx.ravel(), yy.ravel()))
 
+# Classify the points and reshape the result to fit the plot function
 data = hNN_A(gridX)
 data = data.reshape((xplot.shape[0], yplot.shape[0]))
-plt.xticks(np.arange(0, 10, 0.5))
-plt.yticks(np.arange(0, 10, 0.5))
-plt.contourf(xx, yy, data)
 
+# Setup the plot title and axis
+plt.title('Task 3.5 Plot')
+plt.xticks(np.arange(0, 10, 0.5), fontsize=16)
+plt.yticks(np.arange(0, 10, 0.5),fontsize=16)
+plt.xlabel('x1', fontsize=22)
+plt.ylabel('x2', fontsize=22)
+
+# Setup legend
+
+white_patch_legend = mpatches.Patch(color='white', label='Class 0')
+black_patch_legend = mpatches.Patch(color='black', label='Class 1')
+plt.legend(loc='best', fancybox=True, framealpha=0.2, handles=[white_patch_legend, black_patch_legend], facecolor='black', fontsize=16)
+
+# Plot data and show result
+plt.contourf(xx, yy, data, cmap=cm.Greys)
 plt.show()
-
